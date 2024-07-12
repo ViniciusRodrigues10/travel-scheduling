093f3feda574889935a95732f6b7033de0e376f7
Vinicius Rodrigues <viniciusgcr1710@gmail.com>
2024-07-12 17:23:21 -0300

    feat: add travel confirmation route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
 import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import cors from "@fastify/cors"
 
 const app = fastify()
 
@@ -8,6 +10,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip)
+app.register(confirmTrip)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running')
